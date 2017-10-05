0f75ec2c2938b3bc2bab28d8e2edf4300ed936bf
turko222 <cristian.g.aravena@gmail.com>
2017-10-05 15:59:57 -0300

    agregue el imprimir todo de informes

--- duenio.h
+++ duenio.h
@@ -25,4 +25,19 @@ int due_obtenerEspacioLibre(sDuenio  listaDuenios[],int limite);
 int due_generar_Proximo_Id(sDuenio listaDuenios[], int len);
 
 
+/**informes imprimir todo(sEntidad1* array, SEntidad2* array, int lenEntidad1,int lenEntidad2)
+{
+    int indexE2;
+    for (indexE2=0;indexE2<lenEntidad2;indexE2++)
+    {
+        if (arrayEntidad2[indexE2].flag_ocupado == ENTIDAD2_OCUPADO)
+        {
+            int idE1 = arrayEntidad2[indexE2].idEntidad1;
+            int indexE1= enti1_buscarIndicePorId(arrayEntidad1,lenEntidad1,idE1);
 
+            printf("%d\t%s |
+                   %d\t%s , arrayEntidad1[indexE1].id,arrayEntidad1[indexE1].a.arrayEntidad2[indexE2].id")
+        }
+    }
+
+}
